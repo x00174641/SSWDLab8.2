0cd3488ff3071fc5ebd7173aaae6eeafeed296da
X00174641 Jason Fung <x00174641@mytudublin.ie>
2022-11-20 02:21:23 +0000

    SSWD Lab8.2 Startup

---
+++ src/routes/updateproduct/[id]/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { page } from '$app/stores';
+
+    // Sveltekit form enhancements
+    import { enhance, applyAction } from '$app/forms';
+
+    // Import the store etc.
+    import { categories, getAllCategories } from '../../../stores/productStore';
+
+    // Access data returned from +page.server.js
+    // @ts-ignore
+    export let data;
+    // Access data returned from +page.server.js
+    // @ts-ignore
+    export let form;
+
+    // Update list of categories (used in form)
+    getAllCategories();
+
+const { product } = data;
+
+</script>
+  
+  <h1>Params</h1>
+  <h3>id param from url: {$page.params?.id}</h3>
+  <h3>To Do: Update Product with id {product?.id}</h3>
